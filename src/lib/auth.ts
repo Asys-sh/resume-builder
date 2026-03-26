afb2662df6954bdccfc63c20dcd1c9fc12e9fe02
Alexander <96749659+Asys-sh@users.noreply.github.com>
2026-03-26 02:11:51 +0100

    if(auth)
    inthe host, we trust

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,53 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { verify } from '@node-rs/argon2'
-import NextAuth from 'next-auth'
+import NextAuth, { type NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
 import { prisma } from './prisma'
 
+const providers: NextAuthConfig['providers'] = [
+  Credentials({
+    credentials: {
+      email: { type: 'email' },
+      password: { type: 'password' },
+    },
+    async authorize(credentials) {
+      const email = credentials?.email as string | undefined
+      const password = credentials?.password as string | undefined
+      if (!email || !password) return null
+
+      const record = await prisma.password.findUnique({
+        where: { email },
+        include: { user: true },
+      })
+      if (!record) return null
+
+      const valid = await verify(record.hash, password)
+      if (!valid) return null
+
+      return {
+        id: record.user.id,
+        name: record.user.name,
+        email: record.user.email,
+        image: record.user.image,
+      }
+    },
+  }),
+]
+
+if (process.env.AUTH_GOOGLE_ID && process.env.AUTH_GOOGLE_SECRET) {
+  providers.unshift(
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  )
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   secret: process.env.AUTH_SECRET!,
+  trustHost: true,
   session: { strategy: 'jwt' },
 
   pages: {
@@ -27,39 +67,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
 
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-    Credentials({
-      credentials: {
-        email: { type: 'email' },
-        password: { type: 'password' },
-      },
-      async authorize(credentials) {
-        const email = credentials?.email as string | undefined
-        const password = credentials?.password as string | undefined
-        if (!email || !password) return null
-
-        const record = await prisma.password.findUnique({
-          where: { email },
-          include: { user: true },
-        })
-        if (!record) return null
-
-        const valid = await verify(record.hash, password)
-        if (!valid) return null
-
-        return {
-          id: record.user.id,
-          name: record.user.name,
-          email: record.user.email,
-          image: record.user.image,
-        }
-      },
-    }),
-  ],
+  providers,
 
   callbacks: {
     jwt({ token, user }) {
